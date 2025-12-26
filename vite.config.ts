0f596cf385dab292cfe46726665b1f37012c68dc
Aaron Vogt <nyroxsystems@gmail.com>
2025-12-26 14:31:59 +0100

    Fix vite config to resolve index.html correctly

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: './index.html'
+    }
+  },
   server: {
     port: 5173
   }
 });
+
